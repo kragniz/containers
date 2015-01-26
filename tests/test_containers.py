995567c2fb827f6e7e7b6cc7ebb0dfb12a148403
Louis Taylor <kragniz@gmail.com>
2015-01-26 20:22:04 +0000

    Add test structure

---
+++ tests/test_containers.py
@@ -0,0 +1,15 @@
+import unittest
+
+from containers import containers
+
+
+class TestContainers(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
