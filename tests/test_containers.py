e14c53c9ff57f94a1410e51a202561f440c9a1db
Louis Taylor <kragniz@gmail.com>
2015-01-27 22:42:08 +0000

    Add basic http server to test against

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -1,6 +1,31 @@
+try:
+    from http.server import SimpleHTTPRequestHandler
+except ImportError:
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+try:
+    from socketserver import TCPServer
+except ImportError:
+    from SocketServer import TCPServer
+
+import os
+import threading
 import unittest
 
 from containers import containers
+
+PORT = 8080
+
+class TestServer(TCPServer):
+    allow_reuse_address = True
+
+handler = SimpleHTTPRequestHandler
+httpd = TestServer(('', PORT), handler)
+
+httpd_thread = threading.Thread(target=httpd.serve_forever)
+httpd_thread.setDaemon(True)
+httpd_thread.start()
+
 
 class TestDiscovery(unittest.TestCase):
 
